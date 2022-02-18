5f54f991e97d4fc3518958226052101420290143
39mwan <soniaz1998@gmail.com>
2022-02-18 12:57:04 +0100

    created CalculateBalanceTest

--- src/test/java/com/autentia/practica/pot/application/CalculateBalanceTest.java
+++ src/test/java/com/autentia/practica/pot/application/CalculateBalanceTest.java
@@ -1,9 +1,52 @@
 package com.autentia.practica.pot.application;
 
+import com.autentia.practica.pot.dao.ExpenseDao;
+import com.autentia.practica.pot.dao.FakeExpensesDao;
+import com.autentia.practica.pot.model.Expense;
+import com.autentia.practica.pot.model.Friend;
+import com.autentia.practica.pot.service.ExpenseService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CalculateBalanceTest {
+    private ExpenseDao expenseDaoMock;
+    private ExpenseService balanceService;
+    HashMap<Friend, BigDecimal> balance;
+    @BeforeEach
+    public void setUp() {
+        expenseDaoMock = mock(ExpenseDao.class);
+        balanceService = new ExpenseService();
+        balance = new HashMap<>();
+    }
 
 
+    @Test
+    public void shouldReturnBalance(){
+        List<Expense> expenses = new ArrayList<>();
+        Friend luis = new Friend( "luis", "merino");
+        Friend sonia = new Friend( "sonia", "zhang");
 
+        expenses.add(new Expense(luis, BigDecimal.valueOf(20), "taxi", LocalDateTime.now()));
+        expenses.add(new Expense(sonia, BigDecimal.valueOf(10), "comida", LocalDateTime.now()));
+
+        when(expenseDaoMock.getAllExpenses()).thenReturn(expenses);
+
+        CalculateBalance calculateBalance = new CalculateBalance();
+
+        HashMap<Friend, BigDecimal> calculatedBalanceExpected = new HashMap<>();
+        calculatedBalanceExpected.put(luis,BigDecimal.valueOf(5));
+        calculatedBalanceExpected.put(sonia,BigDecimal.valueOf(-5));
+
+        assertEquals(calculatedBalanceExpected,CalculateBalance.calculateBalance());
+    }
 }
