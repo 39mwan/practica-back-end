84c2ca4fdb88bb280c78b95a3d2a02ec602b655d
luismerinou <lmerinoulizarna@gmail.com>
2022-02-21 14:16:46 +0100

    add balance for empty expenses

--- src/main/java/com/autentia/practica/pot/application/CalculateBalance.java
+++ src/main/java/com/autentia/practica/pot/application/CalculateBalance.java
@@ -2,6 +2,7 @@ package com.autentia.practica.pot.application;
 
 import com.autentia.practica.pot.dao.ExpenseDao;
 import com.autentia.practica.pot.dao.FakeExpensesDao;
+import com.autentia.practica.pot.dao.FriendDao;
 import com.autentia.practica.pot.model.Expense;
 import com.autentia.practica.pot.model.Friend;
 import com.autentia.practica.pot.service.ExpenseService;
@@ -14,17 +15,21 @@ import java.util.List;
 //Functional Core, Imperative shell
 //encapsulates business logic in core and a shell (this class) orchestrates DAO and Core
 public class CalculateBalance {
-
     private final ExpenseDao expenseDao;
+    private final FriendDao friendDao;
     private final ExpenseService expenseService;
 
-    public CalculateBalance (ExpenseDao expenseDao, ExpenseService expenseService){
+    public CalculateBalance (ExpenseDao expenseDao, FriendDao friendDao, ExpenseService expenseService){
         this.expenseDao = expenseDao;
         this.expenseService = expenseService;
+        this.friendDao = friendDao;
     }
 
     public HashMap<Friend, BigDecimal> calculateBalance() {
         List<Expense> expenseList = expenseDao.getAllExpenses();
-        return expenseService.calculate(expenseList);
+        if(!expenseList.isEmpty())
+            return expenseService.calculate(expenseList);
+        else
+            return expenseService.calculateIfEmptyExpenses(friendDao.getFriends());
     }
 }
