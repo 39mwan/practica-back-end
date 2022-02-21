d7fac78754a3d6624808fc8ce97488f39f800ee5
luismerinou <lmerinoulizarna@gmail.com>
2022-02-21 14:13:54 +0100

    add AddFriend use case

--- src/main/java/com/autentia/practica/pot/application/AddFriend.java
+++ src/main/java/com/autentia/practica/pot/application/AddFriend.java
@@ -1,4 +1,18 @@
 package com.autentia.practica.pot.application;
 
+import com.autentia.practica.pot.dao.FriendDao;
+import com.autentia.practica.pot.model.Friend;
+
 public class AddFriend {
+
+   private final FriendDao friendDao;
+
+    public AddFriend(FriendDao friendDao) {
+        this.friendDao = friendDao;
+    }
+
+    public boolean addFriendUseCase(Friend friend){
+        return friendDao.insertFriend(friend);
+    }
+
 }
--- src/test/java/com/autentia/practica/pot/application/AddFriendTest.java
+++ src/test/java/com/autentia/practica/pot/application/AddFriendTest.java
@@ -1,7 +1,55 @@
 package com.autentia.practica.pot.application;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.autentia.practica.pot.dao.FakeFriendDao;
+import com.autentia.practica.pot.dao.FriendDao;
+import com.autentia.practica.pot.model.Friend;
+import com.autentia.practica.pot.service.FriendService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 class AddFriendTest {
 
+    private FriendDao friendDaoMock;
+
+    @BeforeEach
+    public void setUp(){
+        friendDaoMock = mock(FakeFriendDao.class);
+    }
+
+    @Test
+    public void shouldAddAValidFriend(){
+        Friend newFriend = new Friend("Luis", "Merino");
+
+        when(friendDaoMock.insertFriend(newFriend)).thenReturn(true);
+
+        AddFriend addFriend = new AddFriend(friendDaoMock);
+        assertTrue(addFriend.addFriendUseCase(newFriend));
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
