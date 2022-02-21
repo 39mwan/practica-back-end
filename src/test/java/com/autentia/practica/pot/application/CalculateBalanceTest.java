8f7cde769e8b76c2898e1f9e75e521de08f9c2a6
luismerinou <lmerinoulizarna@gmail.com>
2022-02-21 11:44:26 +0100

    refactor CalculateBalanceTest

--- src/test/java/com/autentia/practica/pot/application/CalculateBalanceTest.java
+++ src/test/java/com/autentia/practica/pot/application/CalculateBalanceTest.java
@@ -20,34 +20,36 @@ import static org.mockito.Mockito.when;
 
 class CalculateBalanceTest {
     private ExpenseDao expenseDaoMock;
+    private ExpenseService balanceService;
 
     @BeforeEach
     public void setUp() {
         expenseDaoMock = mock(ExpenseDao.class);
-        ExpenseService balanceService = new ExpenseService();
+        balanceService = new ExpenseService();
     }
 
 
     @Test
-    public void shouldReturnBalance(){
+    public void shouldReturnBalance() {
         //Creation of "expenses" = a MOCK DB of ExpenseDao
         List<Expense> expenses = new ArrayList<>();
-        Friend luis = new Friend( "luis", "merino");
-        Friend sonia = new Friend( "sonia", "zhang");
+        Friend luis = new Friend("luis", "merino");
+        Friend sonia = new Friend("sonia", "zhang");
 
         expenses.add(new Expense(luis, BigDecimal.valueOf(20), "taxi", LocalDateTime.now()));
         expenses.add(new Expense(sonia, BigDecimal.valueOf(10), "comida", LocalDateTime.now()));
-        
+
         //simulation of ExpenseDao functionality: expenseDaoMock returns MOCK DB of ExpenseDao
         when(expenseDaoMock.getAllExpenses()).thenReturn(expenses);
 
-        CalculateBalance calculateBalance = new CalculateBalance(expenseDaoMock, new ExpenseService());
+        CalculateBalance calculateBalance = new CalculateBalance(expenseDaoMock, balanceService);
 
 
         HashMap<Friend, BigDecimal> calculatedBalanceExpected = new HashMap<>();
-        calculatedBalanceExpected.put(luis,BigDecimal.valueOf(5));
-        calculatedBalanceExpected.put(sonia,BigDecimal.valueOf(-5));
+        calculatedBalanceExpected.put(luis, BigDecimal.valueOf(5));
+        calculatedBalanceExpected.put(sonia, BigDecimal.valueOf(-5));
 
         assertEquals(calculatedBalanceExpected, calculateBalance.calculateBalance());
+        System.out.println("expected balance: " + calculatedBalanceExpected + "\nactual balance:  " + calculateBalance.calculateBalance());
     }
 }
