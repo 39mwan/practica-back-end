2b5ca99386cea6a4b01ee4cd384fd483360bed31
luismerinou <lmerinoulizarna@gmail.com>
2022-02-21 13:39:31 +0100

    refactor CalculateBalanceTest

--- src/test/java/com/autentia/practica/pot/application/CalculateBalanceTest.java
+++ src/test/java/com/autentia/practica/pot/application/CalculateBalanceTest.java
@@ -21,20 +21,24 @@ import static org.mockito.Mockito.when;
 class CalculateBalanceTest {
     private ExpenseDao expenseDaoMock;
     private ExpenseService balanceService;
+    private Friend luis;
+    private Friend sonia;
 
     @BeforeEach
     public void setUp() {
         expenseDaoMock = mock(ExpenseDao.class);
         balanceService = new ExpenseService();
+
+        luis = new Friend("luis", "merino");
+        sonia = new Friend("sonia", "zhang");
+
     }
 
 
     @Test
-    public void shouldReturnBalance() {
+    public void shouldReturnBalanceForTwoFriends() {
         //Creation of "expenses" = a MOCK DB of ExpenseDao
         List<Expense> expenses = new ArrayList<>();
-        Friend luis = new Friend("luis", "merino");
-        Friend sonia = new Friend("sonia", "zhang");
 
         expenses.add(new Expense(luis, BigDecimal.valueOf(20), "taxi", LocalDateTime.now()));
         expenses.add(new Expense(sonia, BigDecimal.valueOf(10), "comida", LocalDateTime.now()));
@@ -44,6 +48,26 @@ class CalculateBalanceTest {
 
         CalculateBalance calculateBalance = new CalculateBalance(expenseDaoMock, balanceService);
 
+
+        HashMap<Friend, BigDecimal> calculatedBalanceExpected = new HashMap<>();
+        calculatedBalanceExpected.put(luis, BigDecimal.valueOf(5));
+        calculatedBalanceExpected.put(sonia, BigDecimal.valueOf(-5));
+
+        assertEquals(calculatedBalanceExpected, calculateBalance.calculateBalance());
+        System.out.println("expected balance: " + calculatedBalanceExpected + "\nactual balance:  " + calculateBalance.calculateBalance());
+    }
+
+    @Test
+    public void shouldReturnBalanceForRepeatedFriends(){
+        List<Expense> expenses = new ArrayList<>();
+        expenses.add(new Expense(luis, BigDecimal.valueOf(20), "taxi", LocalDateTime.now()));
+        expenses.add(new Expense(sonia, BigDecimal.valueOf(10), "comida", LocalDateTime.now()));
+        expenses.add(new Expense(sonia, BigDecimal.valueOf(10), "comida", LocalDateTime.now()));
+
+        //simulation of ExpenseDao functionality: expenseDaoMock returns MOCK DB of ExpenseDao
+        when(expenseDaoMock.getAllExpenses()).thenReturn(expenses);
+
+        CalculateBalance calculateBalance = new CalculateBalance(expenseDaoMock, balanceService);
 
         HashMap<Friend, BigDecimal> calculatedBalanceExpected = new HashMap<>();
         calculatedBalanceExpected.put(luis, BigDecimal.valueOf(5));
