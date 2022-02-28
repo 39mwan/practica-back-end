e24cf31b87144a9b96970a749f4ba249b9a0c59d
luismerinou <lmerinoulizarna@gmail.com>
2022-02-28 12:48:32 +0100

    added test

--- src/test/java/com/autentia/practica/pot/dao/SQLFriendDaoTest.java
+++ src/test/java/com/autentia/practica/pot/dao/SQLFriendDaoTest.java
@@ -3,12 +3,14 @@ package com.autentia.practica.pot.dao;
 import com.autentia.practica.pot.model.Friend;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@Component
 class SQLFriendDaoTest {
 
     @Autowired
