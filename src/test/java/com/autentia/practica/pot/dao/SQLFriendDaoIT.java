1d2bea4dad20154a3c20af12efd46a860f256613
luismerinou <lmerinoulizarna@gmail.com>
2022-03-03 11:55:56 +0100

    test refactor

--- src/test/java/com/autentia/practica/pot/dao/SQLFriendDaoIT.java
+++ src/test/java/com/autentia/practica/pot/dao/SQLFriendDaoIT.java
@@ -1,37 +1,55 @@
 package com.autentia.practica.pot.dao;
 
 import com.autentia.practica.pot.model.Friend;
+import net.minidev.json.JSONUtil;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest   //Inicia el contexto para inyectar las dependencias
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SQLFriendDaoIT {   //Nomenclatura -IT para tests de integracion
 
     @Autowired
     FriendDao friendDao;
 
-    Friend luis = new Friend("Luis", "Merino");
-    Friend expectedFriend = new Friend("Luis", "Merino");
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
-    List<Friend> friendList = List.of(expectedFriend);
+
+    Friend expectedFriend = new Friend("Luis", "Merino");
+    Friend sonia = new Friend("Sonia", "Zhang");
+    List<Friend> friendList = List.of(expectedFriend, sonia);
 
     @Test
-    void insertFriend() {
-       Friend sonia = new Friend("Sonia", "Zhang");
-       friendDao.insertFriend(sonia);
-        List<Friend> friendList = friendDao.getFriends();
-        assertTrue(friendList.contains(sonia));
+    @Order(1)
+    void getFriends() {
+        assertEquals(friendList, friendDao.getFriends());
+
     }
 
    @Test
-    void getFriends() {
-        assertEquals(friendList, friendDao.getFriends());
-        //System.out.println(friendDao.getFriends());
+   @Order(2)
+    void insertFriend() {
+       friendDao.insertFriend(sonia);
+       List<Friend> expectedList =  jdbcTemplate.query("SELECT * from friends WHERE name ='Sonia' AND surname='Zhang'", (resultSet, rowNumber) -> {
+            Friend friend = new Friend();
+            friend.setName(resultSet.getString("name"));
+            friend.setSurname(resultSet.getString("surname"));
+            return friend;
+        });
+        assertTrue(expectedList.contains(sonia));
     }
+
 }
