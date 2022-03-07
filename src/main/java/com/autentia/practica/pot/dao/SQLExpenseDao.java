a467983416c3e5068dfcbf3fd271134f89244307
luismerinou <lmerinoulizarna@gmail.com>
2022-03-07 13:59:10 +0100

    comment

--- src/main/java/com/autentia/practica/pot/dao/SQLExpenseDao.java
+++ src/main/java/com/autentia/practica/pot/dao/SQLExpenseDao.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
+
 @Repository("SQLExpenseDao")
 public class SQLExpenseDao implements ExpenseDao{
 
@@ -22,12 +25,21 @@ public class SQLExpenseDao implements ExpenseDao{
 
     @Override
     public List<Expense> getAllExpenses() {
-        return null;
+        final String sql = "SELECT * FROM expenses ORDER BY idFriend";
+
+        return jdbcTemplate.query(sql, (resultSet, rowNumber) -> {
+            Expense expense = new Expense();
+            expense.setIdFriend(UUID.fromString(resultSet.getString("idFriend")));
+            expense.setAmount(resultSet.getBigDecimal("amount"));
+            expense.setDescription(resultSet.getString("description"));
+            expense.setDate(resultSet.getTimestamp("date").toLocalDateTime());
+            return expense;
+        });
     }
 
     @Override
     public void addExpense(Expense expense) {
         final String sql = "INSERT INTO expenses VALUES (?, ?, ?, ?)";
-        jdbcTemplate.update(sql, expense.getIdFriend(), expense.getAmount(), expense.getDescription(), expense.getDate());
+        jdbcTemplate.update(sql, expense.getIdFriend().toString(), expense.getAmount(), expense.getDescription(), expense.getDate());
     }
 }
--- src/test/java/com/autentia/practica/pot/dao/SQLExpenseDaoIT.java
+++ src/test/java/com/autentia/practica/pot/dao/SQLExpenseDaoIT.java
@@ -2,18 +2,22 @@ package com.autentia.practica.pot.dao;
 
 import com.autentia.practica.pot.model.Expense;
 import com.autentia.practica.pot.model.Friend;
+import org.apache.tomcat.jni.Local;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,47 +36,84 @@ class SQLExpenseDaoIT {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    //Friend friend = friendDao.getFriends().indexOf(luis);
-
-
     Expense taxiExpense;
-    List<Expense> expenseList;
+    List<Expense> expectedList;
     List<Friend> luisID;
 
-    @BeforeEach
-    void setUp() {
+
+//    @Before
+//    public void setUp() {
+//        friendDao.insertFriend(new Friend("Pepe", "merino"));
+//
+//        luisID = friendDao.getFriends().stream()
+//                .filter(friend -> friend.getName().equals("Pepe"))
+//                .collect(Collectors.toList());
+//
+//        taxiExpense = new Expense(luisID.get(0).getId(), BigDecimal.valueOf(32.10), "Mi taxi", LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+//        expectedList = List.of(taxiExpense);
+////        luisID.forEach(friend -> System.out.println(" ********** " + luisID.get(0).getName() + " "
+////                                                    + luisID.get(0).getId() + " " + luisID.get(0).getSurname()+ "**********" ));
+//
+//        //System.out.println("******* AMOUNT de " + taxiExpense.getIdFriend() +" " +  taxiExpense.getAmount());
+//    }
+
+
+    @Test
+    @Order(1)
+    void addExpense() {
+
         friendDao.insertFriend(new Friend("Pepe", "merino"));
+
         luisID = friendDao.getFriends().stream()
                 .filter(friend -> friend.getName().equals("Pepe"))
                 .collect(Collectors.toList());
-        luisID.forEach(friend -> System.out.println(" ********** " + luisID.get(0).getName() + " "
-                                                    + luisID.get(0).getId() + " " + luisID.get(0).getSurname()+ "**********" ));
 
-        taxiExpense = new Expense(luisID.get(0).getId(), BigDecimal.valueOf(32.1), "Mi taxi", LocalDateTime.now());
-        expenseList = List.of(taxiExpense);
+        //Datos del caso de prueba
+        UUID idLuis = luisID.get(0).getId();
+        BigDecimal amount = new BigDecimal("32.10");
+        String descripcion = "Mi taxi";
+        LocalDateTime fecha = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+
+        taxiExpense = new Expense(idLuis, amount, descripcion, fecha);
+        expectedList = List.of(taxiExpense);
+
+        expenseDao.addExpense(taxiExpense);
+
+        String sql = "SELECT * FROM expenses WHERE idFriend = ? AND amount=? AND description = ? AND date = ?";
+        Expense receivedExpense = jdbcTemplate.queryForObject(sql, (resultSet, rowNumber) -> {
+            Expense expense = new Expense();
+            expense.setIdFriend(UUID.fromString(resultSet.getString("idFriend")));
+            expense.setAmount(resultSet.getBigDecimal("amount"));
+            expense.setDescription(resultSet.getString("description"));
+            expense.setDate(resultSet.getTimestamp("date").toLocalDateTime());
+            return expense;
+        }, idLuis.toString(), amount, descripcion, fecha);
+
+        assertEquals(taxiExpense, receivedExpense);
     }
 
     @Test
     @Order(2)
     void getAllExpenses() {
-        final String sql = "SELECT * FROM expenses ORDER BY idFriend";
-        List<Expense> actualExpenses = jdbcTemplate.query(sql, (resultSet, rowNumber) -> {
-            Expense expense = new Expense();
-            expense.setIdFriend(UUID.fromString(resultSet.getString("idFriend")));
-            expense.setAmount(resultSet.getBigDecimal("amount"));
-            expense.setDescription(resultSet.getString("description"));
-//            expense.setDate(resultSet.("date"));
-            return expense;
-        });
+        //Creacion de resultado esperado : expectedList
+        luisID = friendDao.getFriends().stream()
+                .filter(friend -> friend.getName().equals("Pepe"))
+                .collect(Collectors.toList());
 
-        assertEquals(expenseList, actualExpenses);
+        taxiExpense = new Expense(luisID.get(0).getId(), new BigDecimal("32.10"), "Mi taxi", LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+        expectedList = List.of(taxiExpense);
+
+//        System.out.println("\nLISTA ESPERADA ****************");
+//        expectedList.forEach(expense -> System.out.println(expense.getIdFriend() + " ** "
+//                + expense.getAmount() + " ** " +
+//                expense.getDate() + " ** " +
+//                expense.getDescription() + "\n"));
+//        System.out.println("LISTA REAL DE BD ???????????????????????????????");
+//        expenseDao.getAllExpenses().forEach(expense -> System.out.println(expense.getIdFriend() + " ** "
+//                + expense.getAmount() + " ** " +
+//                expense.getDate() + " ** " +
+//                expense.getDescription() + "\n"));
+
+        assertEquals(expectedList, expenseDao.getAllExpenses());
     }
-
-    @Test
-    @Order(1)
-    void addExpense() {
-        expenseDao.addExpense(taxiExpense);
-        assertTrue(expenseDao.getAllExpenses().contains(taxiExpense));
-    }
-
 }
